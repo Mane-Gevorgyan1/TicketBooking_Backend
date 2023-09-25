f3cb564c828e982318b8aef9b0ac5d9c04761124
Arman Aleqyan <manegevorgyan1@gmail.com>
2023-09-25 19:19:17 +0400

    bug fix

--- router/router.js
+++ router/router.js
@@ -18,25 +18,12 @@ const storage = multer.diskStorage({
 })
 const upload = multer({ storage })
 
-/* ADMIN PANEL  ADMIN PANEL  ADMIN PANEL  ADMIN PANEL */
 
 // Ticket Controller
 router.post('/setSeat', body(['row', 'seat', 'price', 'amphitheater', 'lodge', 'seatColor']).notEmpty().escape(), TicketController.setSeat)
 router.post('/singleTicket', body('ticketNumber').notEmpty().escape(), TicketController.singleTicket)
 router.patch('/changePrice', body(['row', 'seat', 'price', 'amphitheater']).notEmpty().escape(), TicketController.changePrice)
 router.patch('/returnTicket', body('ticketNumber').notEmpty().escape(), TicketController.returnTicket)
-
-// Event Controller
-router.post('/createEvent', upload.single('image'), body([
-    'title', 'location', 'date', 'priceStart', 'priceEnd', 'topEvent',
-    'generalEvent', 'category', 'subcategory', 'place', 'hall', 'genre', 'description'
-]).notEmpty().escape(), EventController.createEvent)
-
-
-
-/* WEBSITE  WEBSITE  WEBSITE  WEBSITE  WEBSITE */
-
-// Ticket Controller
 router.get('/getSeats', TicketController.getSeatDetails)
 router.patch('/changeAvailability', body(['row', 'seat', 'availability', 'amphitheater']).notEmpty().escape(), TicketController.changeAvailability)
 // router.patch('/buyTicket', body([
@@ -45,13 +32,19 @@ router.patch('/changeAvailability', body(['row', 'seat', 'availability', 'amphit
 //     'notes', 'delivery', 'paymentMethod'
 // ]).notEmpty().escape(), TicketController.buyTicket)
 
+
 // Event Controller
+router.post('/createEvent', upload.single('image'), body([
+    'title', 'location', 'date', 'priceStart', 'priceEnd', 'topEvent',
+    'generalEvent', 'category', 'subcategory', 'place', 'hall', 'genre', 'description'
+]).notEmpty().escape(), EventController.createEvent)
 router.get('/getGeneralEvents', EventController.getGeneralEvents)
 router.get('/getTopEvents', EventController.getTopEvents)
 router.get('/randomEvents', EventController.randomEvents)
 router.post('/getAllEvents', EventController.getAllEvents)
 router.get('/singleEvent/:id', EventController.singleEvent)
 router.post('/search', EventController.search)
+
 
 // Category Controller
 router.post('/createCategory', body('name').notEmpty().escape(), CategoryController.createCategory)
--- server.js
+++ server.js
@@ -37,6 +37,13 @@ app.use(session({
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*') // Allow requests from any origin (for development)
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+//   next()
+// })
 
 app.use('/', router)
 
